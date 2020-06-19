1a8d8e7a34918dc8f0ea10f09a1353eaf5e2649b
Kaung-Htet <kaunghtetnyilay@gmail.com>
2020-06-19 22:23:03 +0700

    Separate coding challenge and practice coding

--- src/Main.java
+++ src/Main.java
@@ -24,18 +24,39 @@ public class Main {
 //            }
 //        } while (count !=6);
 
-        int number = 4;
-        int finishNumber = 50;
-        int evenNumRecord = 0;
-        int totalEvenNum = 0;
+//        int number = 4;
+//        int finishNumber = 50;
+//        int evenNumRecord = 0;
+//        int totalEvenNum = 0;
+//
+//        while (number <= finishNumber){
+//            number++;
+//            if (!isEvenNumber(number)){
+//                //continue က break နဲ့ဆင်တယ်
+//                //break ကလုံး၀ရပ်သွားတယ် continue ကအောက်ကကုဒ်တွေဆီဆက်မသွားပဲ
+//                //loop ပြန်ပတ်တယ် (ဆိုလိုတာက next iteration)
+//                //အကယ်၍ continue မပါရင် loop ပတ်တိုင်း အောက်က print statement ဆီရောက်မယ်
+//                continue;
+//            }
+//
+//            System.out.println("Even number " + number);
+//            }
+//
+//        }
 
+
+        // Modify the while code above
+        // Make it also record the total number of even numbers it has found
+        // and break once 5 are found
+        // and at the end, display the total number of even numbers found
+
+    int number = 4;
+    int finishNumber = 50;
+    int evenNumRecord = 0;
+    int totalEvenNum = 0;
         while (number <= finishNumber){
             number++;
             if (!isEvenNumber(number)){
-                //continue က break နဲ့ဆင်တယ်
-                //break ကလုံး၀ရပ်သွားတယ် continue ကအောက်ကကုဒ်တွေဆီဆက်မသွားပဲ
-                //loop ပြန်ပတ်တယ် (ဆိုလိုတာက next iteration)
-                //အကယ်၍ continue မပါရင် loop ပတ်တိုင်း အောက်က print statement ဆီရောက်မယ်
                 continue;
             }
 
@@ -49,11 +70,6 @@ public class Main {
             }
 
         }
-
-        // Modify the while code above
-        // Make it also record the total number of even numbers it has found
-        // and break once 5 are found
-        // and at the end, display the total number of even numbers found
 
     }
 
